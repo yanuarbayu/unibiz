c8d0ce083d5fb3fcbde80de4a9f3aed54a65be6e
farhan <177544242+farhan8883-max@users.noreply.github.com>
2025-10-28 10:28:56 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,168 @@
+ // Data untuk card
+    const dataCard = [
+      {
+        title: "HARIZNA UTAMA ",
+        location: " Kota Banda Aceh",
+        company: "PT. HIMJASA SERTIFIKASI MANDIRI",
+        description: "Salah satu gunung paling indah di Indonesia dengan pemandangan matahari terbit yang menakjubkan.",
+        image: "https://img.freepik.com/free-photo/building_1127-3295.jpg",
+        buttonText: "Jelajahi"
+      },
+      {
+        title: "BANGUN TOBA SEJAHTERA",
+        location: "Kab. Toba Samosie",
+        company: "ASPEKNAS KONSTRUKSI MANDIRI (ASPEKNAS)",
+        description: "Pantai populer di Bali dengan ombak yang cocok untuk berselancar dan matahari terbenam yang indah.",
+        image: "https://img.freepik.com/free-photo/architecture-concept-with-building-plan-smartphone_23-2147813078.jpg",
+        buttonText: "Jelajahi"
+      },
+      {
+        title: "PUSPITA MANDIRI",
+        location: "Kab. Bandung",
+        company: "ASPEKNAS KONSTRUKSI MANDIRI (ASPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/industrial-park-factory-building-warehouse_1417-1923.jpg",
+        buttonText: "Jelajahi"
+      },
+      {
+        
+        title: "CAHAYA RAHMAT TUJUH",
+        location: "Kab. Gowa",
+        company: "ASPEKNAS KONSTRUKSI MANDIRI (ASPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/large-office-buildings_1127-2101.jpg",
+        buttonText: "Jelajahi"
+      },
+      {
+        title: "BINTANG SEJATI",
+        location: "Kab. Majene",
+        company: "SERTIFIKASI BADAN USAHA GABUNGAN PERUSAHAAN KONTRUSI NASIONA (GAPEKSINDO)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/cityscape-office-buildings_1127-3201.jpg",
+        buttonText: "Jelajahi"
+      },
+       {
+        title: "BINTANG SEJATI",
+        location: "Kab. Majene",
+        company: "SERTIFIKASI BADAN USAHA GABUNGAN PERUSAHAAN KONTRUSI NASIONA (GAPEKSINDO)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/premium-photo/engineer-holding-hard-hat-construction-worker-professional-safety-work-industry-building-person-manager-service_10541-902.jpg",
+        buttonText: "Jelajahi"
+      },
+      {
+        title: "BERSAMA ANEKA PROAKTIF",
+        location: "Kab. Indramayu",
+        company: "LSBU GAPEKNAS INFRASTRUKTUR",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/large-modern-office-building_1127-2838.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "CITRAPRASATI KONSORINDO",
+        location: "Kota Bekasi",
+        company: "Aspeknas KONSTRUKSI MANDIRI (ASPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/person-working-building-construction_23-2149184936.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "CITRAPRASATI KONSORINDO",
+        location: "Kota Bekasi",
+        company: "Aspeknas KONSTRUKSI MANDIRI (ASPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/premium-photo/construction-worker-wearing-yellow-hard-hat-safety-vest-is-working-building_548646-136944.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "IQBAL JAYA",
+        location: "Kab. Kolaka",
+        company: "SERTIFIKASI BADAN USAHA GABUNGAN PERUSAHAAN KONTRUSI NASIONA (GAPEKSINDO)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/industrial-park-factory-building-warehouse_1417-1925.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "AIRSOLUSINDO JAYA ANUGRAH",
+        location: "Kab. Bandung",
+        company: "LSBU GAPEKNAS INFRASTRUKTUR (GAPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/building-construction-worker-site-with-architect_23-2149124257.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "DEEPA TIMIRA",
+        location: "Kab. Majalengka",
+        company: "ASPEKNAS KONSTRUKSI MANDIRI (ASPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/sets-cooling-towers-data-center-building_1127-3434.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "ALAMGIR HAKLI INDONESIA",
+        location: "Kab. Sukabumi",
+        company: "ASPEKNAS KONSTRUKSI MANDIRI (ASPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/architecture-concept-with-building-plan-helmet_23-2147813074.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "ACEH MEUGAHNA LINGKE",
+        location: "Kota Banda Aceh",
+        company: "LSBU GAPEKNAS INFRASTRUKTUR (GAPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/agricultural-silos-building-exterior_146671-19100.jpg",
+        buttonText: "Jelajahi"
+      },
+
+       {
+        title: "ACEH MEUGAHNA LINGKE",
+        location: "Kota Banda Aceh",
+        company: "LSBU GAPEKNAS INFRASTRUKTUR (GAPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/coastal-building-destruction-wallpaper_1409-6263.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "DUA PANDAWA",
+        location: "Kab. Parigi Moutong",
+        company: "LSBU KONSTRUKSI INDONESIA (ASPEKINDO)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/analog-landscape-city-with-buildings_23-2149661463.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "MAZEL ARNAWAMA INDONESIA",
+        location: "Kota Bandung",
+        company: "LSBU GAPEKNAS INFRASTRUKTUR (GAPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/smart-asian-male-professional-civil-engineer-hand-hold-laptop-site-construction-check-with-infastrcture-building-background_609648-979.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "DWTECHNIC",
+        location: "Kab. Adm. Jakarta Selatan",
+        company: "LSBU GAPEKNAS INFRASTRUKTUR (GAPEKNAS)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/beautiful-exterior-building-architecture-brick-warehouse_74190-1077.jpg",
+        buttonText: "Jelajahi"
+      },
+
+      {
+        title: "GALIH CITRA MANDIRI",
+        location: "Kab. Bangli",
+        company: "Gamana Krida Bhakti(GAPENSI)",
+        description: "Surga bawah laut di Papua yang terkenal dengan keindahan terumbu karangnya.",
+        image: "https://img.freepik.com/free-photo/entrance-modern-office-building_1127-2295.jpg",
+        buttonText: "Jelajahi"
+      }
+    ];
